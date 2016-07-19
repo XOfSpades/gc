b81a4551c3ff260a56951f2853a8cf108587a848
XOfSpades <bernhard.stoecker@invision.de>
2016-07-19 22:22:23 +0200

    Initial commit

---
+++ lib/gc.ex
@@ -0,0 +1,18 @@
+defmodule Gc do
+  use Application
+
+  def start( _type, _args ) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(__MODULE__, [], function: :run)
+    ]
+
+    opts = [strategy: :one_for_one, name: Gc.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def run do
+    { :ok, _ } = Plug.Adapters.Cowboy.http Gc.Router, []
+  end
+end
---
+++ test/gc_test.exs
@@ -0,0 +1,8 @@
+defmodule GcTest do
+  use ExUnit.Case
+  doctest Gc
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
