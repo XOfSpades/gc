d9a74879729599e6966f0de559e84d53e37b8158
XOfSpades <stoecki@der-ball-ist-rund.net>
2016-07-22 01:01:55 +0200

    Extend Consumption module by .to_json and .changeset

--- lib/gc/energy_consumption.ex
+++ lib/gc/energy_consumption.ex
@@ -1,10 +1,28 @@
 defmodule Gc.EnergyConsumption do
+  @derive [Poison.Encoder]
   use Ecto.Schema
+  alias Ecto.Changeset
 
   schema "energy_consumptions" do
     field :device_name,  :string
     field :consumption,  :float
     field :from,         Ecto.DateTime
     field :to,           Ecto.DateTime
+
+    timestamps
+  end
+
+  @required_fields [:device_name, :consumption, :from, :to]
+
+  def to_json(model) do
+    model
+    |> Map.take([:device_name, :consumption, :from, :to, :id])
+    |> Poison.encode!
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> Changeset.cast(params, @required_fields)
+    |> Changeset.validate_required(@required_fields)
   end
 end
