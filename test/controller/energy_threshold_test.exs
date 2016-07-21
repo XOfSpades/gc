25949e005b3d3cdbedea6e9a5b6b5be3b079740b
XOfSpades <stoecki@der-ball-ist-rund.net>
2016-07-22 00:30:47 +0200

    Use valid json body in put threshold test

--- test/controller/energy_threshold_test.exs
+++ test/controller/energy_threshold_test.exs
@@ -6,7 +6,7 @@ defmodule Gc.Controller.EnergyThresholdTest do
 
   describe("update threshold") do
     test "It responds with 200 OK" do
-      conn = conn(:put, "/api/energy_threshold", "")
+      conn = conn(:put, "/api/energy_threshold", "{}")
       |> Gc.Router.call(@opts)
 
       assert conn.status == 200
