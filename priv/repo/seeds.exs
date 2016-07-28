10b79a38dcb232f12cd864f36c933c6d92c68cfe
XOfSpades <stoecki@der-ball-ist-rund.net>
2016-07-28 15:00:05 +0200

    Add further Device seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -8,3 +8,10 @@ Mix.shell.info "Seeding Application..."
 
 Mix.shell.info "Created user:"
 Mix.shell.info Model.to_json(user)
+
+{:ok, device} = Model.changeset(
+  %Gc.Device{}, %{name: "Device", user_id: user.id})
+|> Repo.insert
+
+Mix.shell.info "Created device:"
+Mix.shell.info Model.to_json(device)
